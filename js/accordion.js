9dd3a388aa35e2d82912f90dc99b849d07cd22fb
Anyanime Divine <anyanimedivine559@gmail.com>
2023-12-23 16:09:19 +0100

    chore: about page styling and responsiveness done✅✅

---
+++ js/accordion.js
@@ -0,0 +1,19 @@
+
+
+const aOpen = document.getElementById("accordion-open");
+const aClose = document.getElementById("accordion-close");
+const aContent = document.getElementById("accordion-content");
+if ((aOpen.style.display = "block")) {
+  aClose.style.display = "none";
+}
+aOpen.addEventListener("click", () => {
+  aOpen.style.display = "none";
+  aClose.style.display = "block";
+  aContent.style.display = "block";
+});
+
+aClose.addEventListener("click", () => {
+  aOpen.style.display = "block";
+  aClose.style.display = "none";
+  aContent.style.display = "none";
+});
